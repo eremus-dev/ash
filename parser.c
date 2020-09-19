a78878aaaaf009ccb67f08ce1056011c921aebc5
Nathan <bryslandnathan75@gmail.com>
2020-09-19 18:07:39 +0800

    code cleanup and commenting

--- parser.c
+++ parser.c
@@ -1,11 +1,12 @@
 #include "parser.h"
 
-int parse_commandline(char * commandline, JOB * job_queue){
-    
+int parse_commandline(char * commandline, JOB * job_queue)
+{
+    return 0;
 }
 
-void abort_parsing(int job_count, JOB * queue){
-
+void abort_parsing(int job_count, JOB * queue)
+{
     return;
 }
 
--- test.c
+++ test.c
@@ -3,25 +3,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool example_test(void);
+/*bool example_test(void);*/
 
 int main(void) {
         
     int fail = 0;
     int success = 0;
 
-     if(example_test()){
-         success++;
-     } else {
-         fail++;
-     }
+    
+
+    /*if(example_test()){
+        success++;
+    } else {
+        fail++;
+    }*/
 
     printf("Test passed %d\nTest failed %d\n", success, fail);
 
     return 0;
 }
 
-bool example_test(void){
+/*bool example_test(void){
 
     if(true){
         printf("Test example_test: test passed\n");
@@ -31,4 +33,4 @@ bool example_test(void){
         return 1;
     }
 
-}
+}*/
