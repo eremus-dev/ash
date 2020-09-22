4b32898bf65dc969fe0c204ae18da30a61637c9c
sam <sam@eremus.dev>
2020-09-22 11:18:25 +0800

    get_commandline function

--- ashell.c
+++ ashell.c
@@ -1,20 +1,16 @@
 #include "ashell.h"
 
-void get_commandline(char * commandline)
-{
-    return;
-}
 
-void exec_command(Command * command_queue)
-{
-    return;
-}
 
 void exec_shell() {
 
     JOB job_queue[MAX_JOBS];
     char * tokens[MAX_ARG_LEN]; 
-    char commandline[MAX_COMMAND_LEN]; 
+    
+    char * commandline = malloc(MAX_COMMAND_LEN);
+    if(commandline == NULL){
+        perror("Error: Commandline allocation failure");
+    }
 
     while(true){
         get_commandline(commandline);
@@ -32,3 +28,24 @@ void exec_shell() {
     return;
 }
 
+// Function partly inspired by write up: https://brennan.io/2015/01/16/write-a-shell-in-c/
+void get_commandline(char * commandline)
+{
+    int check = getline(commandline, MAX_COMMAND_LEN, stdin);
+
+    if(check == -1){
+        if(feof(stdin)){
+            exit(0);
+        } else {
+            perror("ERROR: In get_commandline, getline failed.");
+            exit(-1);
+        }
+    }
+ 
+    return;
+}
+
+void exec_command(Command * command_queue)
+{
+    return;
+}
