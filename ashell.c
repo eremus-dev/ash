2997a4560ea46fc3ebda3e3612fb33831440f0ed
sam <sam@eremus.dev>
2020-09-28 16:51:08 +0800

    remove newline printed with prompt

--- ashell.c
+++ ashell.c
@@ -126,7 +126,7 @@ int handle_redirection(command * com, int * in, int * out, int * off, int * pipe
 
 void print_prompt(char * prompt)
 {
-    printf("\n\033[1;32m%s\033[0m ", prompt);
+    printf("\033[1;32m%s\033[0m ", prompt);
 }
 
 void exit_shell(int stat)
