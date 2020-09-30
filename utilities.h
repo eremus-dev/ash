f47aea20093faf4f7ab1d94431d5bbf00d72cc36
sam <sam@eremus.dev>
2020-09-30 16:05:10 +0800

    moved functions around

--- utilities.h
+++ utilities.h
@@ -46,5 +46,22 @@ int pwd_command(command * com);
  */
 int cd_command(command * com);
 
+/**
+ * Function to print the prompt set in the main.c 
+ * 
+ * Can be changed by calling function prompt with an argument or without for interactive change.
+ * 
+ * Arguments:
+ *      prompt - prompt buffer
+ * 
+ * Returns:
+ *      void
+ */
+void print_prompt(char * prompt);
+
+/**
+ * Function to print exit on exit, probably shouldn't exist.
+ */
+void exit_shell(int stat);
 
 #endif
